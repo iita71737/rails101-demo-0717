1ac1e84dd8bb9c73b5be2fb6f84a30a182944cef
Dan <iita71737@gmail.com>
2016-07-17 15:12:43 +0800

    for deploy heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
+
+namespace :account do
+  resources :groups
+  resources :posts
+end
+
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :groups do
+    member do
+      post :join
+      post :quit
+    end
 
+    resources :posts
+  end
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'groups#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
